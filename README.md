54ce275568c5b503c039c05b480ea41964b5aec4
Steve Melia <steve.j.melia@gmail.com>
2016-12-19 16:51:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# electron-boilerplate-sqlite
+
+Truly tiny boilerplate for an Electron app, including sqlite. Cut down from https://github.com/szwacz/electron-boilerplate
+
+```
+git clone https://github.com/sjmelia/electron-boilerplate-sqlite.git
+cd electron-boilerplate-sqlite
+npm install
+npm start
+```
+
+## Building a release package
+
+Releases can only be built on the target platform.
+
+`npm run release`
+
+## Using native modules
+
+If you wish to use native modules, you must run `npm run postinstall` after first install of the module.
---
+++ app/app.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+
+const app = electron.app;
+
+var BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
+
+
