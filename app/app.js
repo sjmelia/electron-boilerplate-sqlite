239f499c97a45275b9409d8778478c9efb56abf2
Tony Diep <tonydiep@tonydiep.com>
2019-07-12 15:52:53 -0400

    fix database not loading

--- app/app.js
+++ app/app.js
@@ -6,26 +6,33 @@ var BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-app.on('window-all-closed', function() {
-    app.quit();
+app.on('window-all-closed', function () {
+  app.quit();
 });
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
   });
 });
-
-
